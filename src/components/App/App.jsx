cb8fbdc7fc07b1ab8dc108b458cdeea88acb4705
arjdev69 <bruno96araujo@gmail.com>
2019-12-18 17:05:01 -0300

    create componet app

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class App extends Component {
+  render() {
+    return(
+        <div>
+            App
+        </div>
+    );
+  }
+}
