0588eb45b2fae27b4772660f07371b28c2de86cd
arjdev69 <bruno96araujo@gmail.com>
2019-12-18 19:14:32 -0300

    update component about

---
+++ src/components/About/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const About = props => (
+    <div>
+        About
+    </div>
+);
+
+export default About;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 // import { Container } from './styles';
+import { About } from '../'
 
 export default class App extends Component {
   render() {
     return(
         <div>
             App
+            <About />
         </div>
     );
   }
