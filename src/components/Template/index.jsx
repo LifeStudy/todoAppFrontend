d96986937811e2cd308b49b9526c0cefbcdd60d8
arjdev69 <bruno96araujo@gmail.com>
2019-12-19 13:28:58 -0300

    change name component If

--- src/components/Template/index.jsx
+++ src/components/Template/index.jsx
@@ -1,4 +1,4 @@
-export { default as IF } from './If';
+export { default as If } from './If';
 export { default as Grid } from './Grid';
 export { default as IconButton } from './IconButton';
 export { default as Menu } from './Menu';
