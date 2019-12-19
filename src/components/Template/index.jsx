7b5bc7c8d5c22aac3ec7ec2b49de0885ebd7c298
arjdev69 <bruno96araujo@gmail.com>
2019-12-19 12:08:50 -0300

    created components Template

---
+++ src/components/Template/index.jsx
@@ -0,0 +1,5 @@
+export { default as IF } from './If';
+export { default as Grid } from './Grid';
+export { default as IconButton } from './IconButton';
+export { default as Menu } from './Menu';
+export { default as PageHeader } from './PageHeader';
