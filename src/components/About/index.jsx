c89eba410538e349162dde93c9da85fc50ffda67
arjdev69 <bruno96araujo@gmail.com>
2019-12-19 13:28:01 -0300

    done about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { PageHeader } from '../Template';
 
 const About = props => (
     <div>
-        About
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
     </div>
 );
 
